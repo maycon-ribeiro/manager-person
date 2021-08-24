90b36853ffe2a839cb48f057a39eca9e01913a55
mayquiler <mayconrodriguesribeiro@gmail.com>
2021-08-24 16:23:58 -0300

    Primeiro commit

---
+++ src/main/java/com/manager/managerperson/ManagerPersonApplication.java
@@ -0,0 +1,13 @@
+package com.manager.managerperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerPersonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manager/managerperson/ManagerPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.managerperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
